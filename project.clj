cb7797e358ed5e3cff7fc68ae918cb6277c93e3f
Henry Garner <henry@henrygarner.com>
2012-07-25 11:19:23 +0100

    Update for lein 2

--- project.clj
+++ project.clj
@@ -4,10 +4,8 @@
                  [org.apache.mahout/mahout-core "0.5"]
                  [org.apache.mahout/mahout-math "0.5"]
                  [org.apache.mahout/mahout-utils "0.5"]]
-  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]]
-  ;; :source-path "src/clj"
-  :java-source-path [["src/java"]]
-  :java-options {:debug "true"}
+  :source-path "src/clj"
+  :java-options { :debug "true" }
   :jvm-opts ["-Xmx768m" "-d64" "-server" ;; "-XX:+NewRatio=12"
-             "-XX:+UseParallelGC" "-XX:+UseParallelOldGC"]
+              "-XX:+UseParallelGC" "-XX:+UseParallelOldGC"]
   :main mahoutinaction.core)
